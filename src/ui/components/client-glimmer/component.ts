6318298cee1b7e41bbe591beab9b4244d7084b19
Jordan Cauley <jordan@cauley.co>
2017-04-13 16:58:49 -0400

    init commit

--- src/ui/components/client-glimmer/component.ts
+++ src/ui/components/client-glimmer/component.ts
@@ -1,5 +1,19 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class ClientGlimmer extends Component {
+
+  brewery: any;
+
+  constructor(options) {
+    super(options);
+    this.loadBrewery();
+  }
+
+  async function loadBrewery() {
+    let request = await fetch('https://app.beerncapp.com/api/v1/breweries/100');
+    let json = await request.json();
+    console.log(json)
+    this.brewery = json.brewery;
+  }
 
 }
